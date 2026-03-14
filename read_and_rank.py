c83cc9083706230260c2c3657626a5f83d043b95
Alexandre Tilly <agtaz19@gmail.com>
2026-03-14 00:06:29 -0400

    sample data and primary imput file

---
+++ read_and_rank.py
@@ -0,0 +1,17 @@
+"""DO NOT CHANGE CODE"""
+import sys
+
+if sys.version_info.major != 3 or sys.version_info.minor != 11:
+    print("-" * 60)
+    print("ENVIRONMENT ERROR: Python 3.11 is required for this assessment.")
+    print(f"You are currently running: {sys.version.split()[0]}")
+    print("Please switch to Python 3.11 to avoid compatibility issues with the grader.")
+    print("-" * 60)
+    sys.exit(1)
+
+class Company:
+    def __init__(self, data):
+    #YOUR CODE BELOW:
+        """ ^DO NOT CHANGE CODE ABOVE^ """
+
+
